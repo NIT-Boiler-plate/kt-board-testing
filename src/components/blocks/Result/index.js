31327f56d0fa0675b166b6bc5825788748914ca3
namil kim <namil.kim@kt.com>
2024-08-20 10:42:21 +0900

    안드로이드 다운로드 시 집계기능 추가

--- src/components/blocks/Result/index.js
+++ src/components/blocks/Result/index.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import Result from './Result';
 import { useRecoilState } from 'recoil';
-import { boardState, imageUrlState } from '../../../store/stateAtoms';
+import { boardState, imageUrlState, positionState, userState } from '../../../store/stateAtoms';
+import { addDoc, collection } from 'firebase/firestore';
+import { dbService } from '../../../firebase';
+import { DAO } from '../../../util/data';
 
 // 결과물을 보여주는 컴포넌트
 const Index = ({ imageRef }) => {
+  const [userData, setUserData] = useRecoilState(userState);
   const [boardData, setBoardData] = useRecoilState(boardState);
+  const [positionData, setPositionData] = useRecoilState(positionState);
   const [imageUrl, setImageUrl] = useRecoilState(imageUrlState);
 
-  const handleDownload = () => {
+  const { branchType, centerType, latestBoardType, email, dockey, name, team, uid } = userData;
+  const postForm = {
+    GPSLatitude: positionData['SELECT'].latitude ? positionData['SELECT'].latitude : positionData['GPS'].latitude,
+    GPSLongitude: positionData['SELECT'].longitude ? positionData['SELECT'].longitude : positionData['GPS'].longitude,
+    branchType: branchType,
+    centerType: centerType,
+    boardType: DAO(latestBoardType),
+    email: email,
+    dockey: dockey,
+    madeBy: name,
+    team: team,
+    uid: uid,
+    createAt: new Date(),
+    postItems: [],
+  };
+
+  const handleDownload = async () => {
     const isIOS = /iPhone|iPad|iPod/i.test(navigator.userAgent);
     const isAndroid = /Android/i.test(navigator.userAgent);
 
@@ -23,8 +44,10 @@ const Index = ({ imageRef }) => {
     }
 
     if (window.confirm('이미지를 저장하시겠습니까?')) {
+      const _postData = { ...postForm, postItems: [...boardData] };
+      console.log('다운로드 실행');
+      const docRef = await addDoc(collection(dbService, process.env.REACT_APP_FIREBASE_DOWNLOAD_COLLECTION), _postData);
     } else {
-      console.log('취소');
       return;
     }
 
--- src/components/blocks/Share/index.js
+++ src/components/blocks/Share/index.js
@@ -32,8 +32,6 @@ const Index = ({ imageRef }) => {
   const handleShare = async () => {
     // board를 DB에 넣기 알맞은 형태로 바꿔줘야함.
 
-    // await setDoc(doc(dbService, 'REACT_APP_FIREBASE_POST_COLLECTION', dockey), _postData);
-
     if (!navigator.share) {
       alert('공유 기능을 사용할 수 없는 환경입니다.\n*안드로이드는 크롬브라우저, 아이폰은 사파리를 사용해주세요.');
       return;
